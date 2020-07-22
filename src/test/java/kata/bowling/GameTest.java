f43b74a2c66c2cd191707c2b559e8c4658275e68
Adrian Munteanu <adrianmunteanu-gh@users.noreply.github.com>
2020-07-22 21:34:35 +0300

    Initial commit

---
+++ src/test/java/kata/bowling/GameTest.java
@@ -0,0 +1,48 @@
+package kata.bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
+
+public class GameTest {
+	Game game;
+
+	@Before
+	public void setUp() {
+		game = new Game();
+	}
+
+	@Test
+	public void CanScoreGutterGame() {
+		game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		assertThat(game.score(), is(0));
+	}
+
+	@Test
+	public void canScoreGameOfOnes() {
+		game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+		assertThat(game.score(), is(20));
+	}
+
+	@Test
+	public void canScoreSpaceFollowedByThhree() {
+		game.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		assertThat(game.score(), is(16));
+	}
+	
+	@Test
+	public void canScoreStrikeFollowedByThreeThenThree() {
+		game.roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		assertThat(game.score(), is(22));
+	}
+	
+	@Test
+	public void canScorePerfectGame() {
+		game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+		assertThat(game.score(), is(300));
+	}
+
+}
